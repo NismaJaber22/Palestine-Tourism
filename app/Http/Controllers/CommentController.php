6fea8e070c4359ca00a1bf08c30634d7f9a595dc
NismaJaber22 <nismaj87@gmail.com>
2022-12-18 23:28:44 +0200

    edit booking , seeder  &  comments

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function createComment(Request $request)
+    {
+
+        $data = $request->validate([
+            'comment' => "min:2",
+            'user_id' => "required|exists:users,id",
+            'blog_id' => "required|exists:blogs,id"
+        ]);
+
+        $comment = Comment::create($data);
+        return redirect('/BlogsHome');
+        // return  $comment;
+        // Comment::create([
+        //     'comment'=>$request->comment,
+        //     'user_id' => $request-> user_id,
+        //     'blog_id' => $request->blog_id
+        // ]);
+
+    }
+}
